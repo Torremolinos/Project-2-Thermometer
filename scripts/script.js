ca572eaaaa1816e527644fc51b369913c7f1391e
Torremolinos <adrian_2054@hotmail.com>
2023-09-21 21:27:17 +0200

    Añadiendo comentarios y arreglando la ultima funcion del script

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+/*Creamos una arrow function de cambio de celsius a farenheit
+con un if para que el usuario no meta nada que no sea un numero
+dado que el usuario metera un numero en forma de string cambiamos ese tipo a numero.*/
 const cambio =() =>{
     const celsius = parseFloat(document.getElementById('celsius').value);
     console.log(typeof(celsius));
@@ -5,7 +8,8 @@ const cambio =() =>{
         alert('Por favor, ingresa una temperatura válida')
         return;
     }
+    //Creamos una constante para los farenheit que a su vez es la propia ecuacion
     const f = (celsius*9/5)+32;
-    
-    document.getElementById('f').textContent= f.toFixed;
+    //Para terminar le decimos al html que con el id f salga el contenido redondeado a 2 cifras.
+    document.getElementById('f').textContent= f.toFixed(2);
 }
